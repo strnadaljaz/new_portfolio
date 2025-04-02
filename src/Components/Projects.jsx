eed24d05589c13d79b43df513e6028fa6d1828a0
astrnad1 <strnadaljaz23@gmail.com>
2025-04-02 15:34:01 +0200

    new

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -7,19 +7,25 @@ export default function Projects() {
     ];
 
     return (
-        <div id="Projects" className="bg-[#2C2F36] h-[80vh] flex flex-wrap justify-center items-center gap-6 p-6">
-            {
-                projects.map((project) => (
-                    <div className="bg-white text-black h-[40vh] w-80 rounded-lg shadoww-lg p-4 text-center transition-transform transform hover:scale-105">
-                        <img src={project.image} alt="project-image" className="w-full h-40 object-cover rounded-t-lg" />
-                        <h1 className="text-xl font-semibold mt-3">{project.title}</h1>
-                        <p className="text-gray-600 text-sm mt-2">{project.description}</p>
-                        <button className="mt-4 bg-blue-500 hover:bg:blue-600 text-white font-medium px-4 py-2 rounded-lg transition duration-300">
-                            <a href={project.link} target="_blank" rel="noopener noreferrer">View on Github</a>
-                        </button>
-                    </div>
-                ))
-            }
+        <div id="Projects" className="bg-[#2C2F36] h-[80vh]">
+            <div className="flex justify-center items-center" >
+                <h1>MY PROJECTS</h1>
+            </div>
+            <div className="flex flex-wrap justify-center items-center gap-6 p-6">
+                {
+                    projects.map((project) => (
+                        <div key={project.title} className="bg-white text-black h-[50vh] w-80 rounded-lg shadoww-lg p-4 text-center transition-transform transform hover:scale-105">
+                            <img src={project.image} alt="project-image" className="w-full h-40 object-cover rounded-t-lg" />
+                            <h1 className="text-xl font-semibold mt-3">{project.title}</h1>
+                            <p className="text-gray-600 text-sm mt-2">{project.description}</p>
+                            <button className="btn btn-outline border-[#FFD700] text-[#FFD700] hover:bg-[#FFD700] hover:text-black">
+                                <a href={project.link} target="_blank" rel="noopener noreferrer">View on Github</a>
+                            </button>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
+
     );
 }
