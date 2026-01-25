73f28e759f55d8cd40975d38bc607711000da79b
strnadaljaz <strnadaljaz23@gmail.com>
2026-01-25 20:15:09 +0100

    fix

--- src/app/webpages/page.tsx
+++ src/app/webpages/page.tsx
@@ -12,7 +12,7 @@ export default function Webpages () {
                     <span className="text-transparent bg-clip-text bg-gradient-to-r from-white to-[#FFD700]">Need a Webpage?</span>
                 </h1>
                 <p className="text-lg md:text-2xl text-gray-300 mb-8 max-w-2xl text-center">
-                    I design and build modern, fast, and user-friendly websites tailored to your needs. Whether you need a portfolio, business site, or custom web app, let's bring your ideas to life!
+                    I design and build modern, fast, and user-friendly websites tailored to your needs. Whether you need a portfolio, business site, or custom web app, let&apos;s bring your ideas to life!
                 </p>
                 <a href="mailto:aljaz.strnad@student.um.si" className="bg-[#FFD700] text-black font-bold rounded-full px-8 py-4 text-xl shadow-lg hover:bg-yellow-400 transition-all duration-300">
                     Contact Me
